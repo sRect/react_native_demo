4b0eb7035c54fbad157fa5eded61d62140524f9c
srect <fcqun@sina.cn>
2022-09-04 22:04:36 +0800

    feat: interpolate

--- pages/AnimateDemo/index.js
+++ pages/AnimateDemo/index.js
@@ -1,10 +1,51 @@
 import React, {memo} from 'react';
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  useWindowDimensions,
+  Pressable,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    backgroundColor: '#fcfcfc',
+  },
+  ball: (width, height) => ({
+    width: 100,
+    height: 100,
+    borderRadius: 100,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'blue',
+    position: 'absolute',
+    top: height / 1.5,
+    left: width / 2 - 50,
+    overflow: 'hidden',
+  }),
+  txt: {
+    color: '#ffffff',
+    fontFamily: 'lucida grande',
+    fontSize: 24,
+  },
+});
 
 const AnimateDemo = () => {
+  const {width: windowWidth, height: windowHeight} = useWindowDimensions();
+
+  const handlePress = () => {
+    console.log('press');
+  };
+
   return (
-    <View>
-      <Text>animate</Text>
+    <View style={styles.wrapper}>
+      <Pressable onPress={handlePress}>
+        <View style={styles.ball(windowWidth, windowHeight)}>
+          <Text style={styles.txt}>click</Text>
+        </View>
+      </Pressable>
     </View>
   );
 };
